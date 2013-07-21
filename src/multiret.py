e67d6779abf07fc90cf1f92be6cd133491abe74a
HU Pili <hpl1989@gmail.com>
2013-07-21 21:17:54 +0800

    drafted multiret

--- src/multiret.py
+++ src/multiret.py
@@ -1,22 +1,46 @@
 from functools import wraps
 import inspect
+import re
 
-def multi(f1, f2):
-    def liu(func):
-        @wraps(func)
-        def _(*args, **kwargs):
-            frame,filename,line_number,function_name,lines,index=\
-                inspect.getouterframes(inspect.currentframe())[1]
-            call_str = lines[0].strip()
-            #return execute_method_for(call_str)
-            print 'call_str:', call_str
-            return func()
-        return _
-    return liu
+PATTERN_MULTI_RETURN = r'(\w+(\s*,\s*\w+)*)\s*=\s*%s'
 
-@multi(1,2)
-def f(*args, **kwargs):
-    return 1, 2
+def multi(func):
+    @wraps(func)
+    def wrapped_func(*args, **kwargs):
+        mypattern = PATTERN_MULTI_RETURN % func.__name__
+        frame, filename, line_number, function_name, lines, index = \
+            inspect.getouterframes(inspect.currentframe())[1]
+        call_str = lines[0].strip()
+        print 'call_str:', call_str
+        ret = re.search(mypattern, call_str)
+        if ret:
+            expected_vars = map(str.strip, ret.groups()[0].split(','))
+            print expected_vars
+        else:
+            expected_vars = ['x']
+        returned_vars = func(*args, **kwargs)
+        if len(expected_vars) == 1:
+            if isinstance(returned_vars, tuple):
+                return returned_vars[0]
+            else:
+                return returned_vars
+        else:
+            if not isinstance(returned_vars, tuple):
+                returned_vars = (returned_vars, )
+            if len(expected_vars) <= len(returned_vars):
+                return returned_vars[0:len(expected_vars)]
+            else:
+                return returned_vars + (None, ) * (len(expected_vars) - len(returned_vars))
+    return wrapped_func
 
-print f()
-a, b = f()
+
+if __name__ == '__main__':
+    @multi
+    def f(*args, **kwargs):
+        return 1, 2
+    print f()
+    a, b = f()
+    print a, b
+    a, b, c = f()
+    print a, b, c
+
